672d9e129598d722a8bd3a6637a2c86af6107f46
kapiltan <kapiltanwarhsr0@gmail.com>
2023-01-15 14:02:50 +0530

    Challenge 7

--- app.js
+++ app.js
@@ -28,12 +28,9 @@ app.get("/contact", function(req,res){
   res.render("contact",{contactContent:contactStartingContent});
 })
 
-
-
-
-
-
-
+app.get("/compose", function(req,res){
+  res.render("compose");
+})
 
 
 
